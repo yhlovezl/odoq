86294270721104e969d327d5648616c51d732fbb
yanghongliang <2357574009@qq.com>
2019-10-10 11:04:18 +0800

    测试

---
+++ src/main/java/com/xiaoniucr/App.java
@@ -0,0 +1,12 @@
+package com.xiaoniucr;
+
+/**
+ * @author yanghl
+ * @date 2019/10/10 11:03
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
